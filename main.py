08017f1daaac34a3dcfbe8f00e3b3a0a815b0af7
Aidan Newman <aidannewm@gmail.com>
2024-04-01 21:16:08 -0400

    main.py options menu
    Created a main.py input prompt to prevent constant change of the main
    function. Prompts user to start a live video analysis, to select an
    input video for analysis, or select an input image for analysis.

--- main.py
+++ main.py
@@ -1,36 +1,103 @@
+import time
 import paths
 from tasks import analysis
 from neural_network.predict import load_corrections_model, load_identify_model
-from pathlib import Path
-from tasks import image
 
 
 def main(*args, **kwargs):
 
-    # testing
-    correction_model = load_corrections_model()
-    identify_model = load_identify_model()
-    
-    analysis.analyze_video(
-        filepath=paths.INPUT_VIDEOS / "vid.mp4",
-        correction_model=correction_model,
-        identify_model=identify_model,
-        output_window=True,
-        annotate=True,
-        save_file=paths.OUTPUT_VIDEOS / "output_video.avi"
-    )
+    print("Waiting for TensorFlow...")
+    time.sleep(2)
 
-    # count = 0
-    # for img in paths.INPUT_IMAGES.iterdir():
-    #     analysis.analyze_image(img=image.load(img),
-    #                            identify_model=idt_model,
-    #                            correction_model=correction_model,
-    #                            window=True,
-    #                            hold=True,
-    #                            annotate=True,
-    #                            count = count
-    #                            )
-    #     count += 1
+    while True:
+        entry = input("Select an option:\n"
+                      "[1] Analyze live video\n"
+                      "[2] Analyze a stored video\n"
+                      "[3] Analyze a stored image\n")
+
+        # input check
+        if str.isdigit(entry):
+            entry = int(entry)
+            if entry == 1 or entry == 2 or entry == 3:
+
+                # create models
+                correction_model = load_corrections_model()
+                identify_model = load_identify_model()
+
+                # start analyzing live video
+                if entry == 1:
+                    analysis.analyze_video(
+                        correction_model=correction_model,
+                        identify_model=identify_model,
+                        output_window=True,
+                        annotate=True
+                    )
+
+                # start analyzing a selected input video
+                elif entry == 2:
+
+                    names = []
+
+                    # prompt user to select input video file
+                    output = "Select an input video:\n"
+                    i = 1
+                    for suf in ("*.mp4", "*.mov", "*.avi"):
+                        for file in paths.INPUT_VIDEOS.glob(suf):
+                            output += ("[" + str(i) + "] " + file.name + "\n")
+                            names.append(file.name)
+                            i += 1
+
+                    if len(names) == 0:
+                        print("No input videos available.")
+                        break
+                    while True:
+                        entry = input(output)
+                        # input check
+                        if str.isdigit(entry):
+                            entry = int(entry)
+                            if 1 <= entry <= i:
+                                analysis.analyze_video(
+                                    filepath=paths.INPUT_VIDEOS / names[entry-1],
+                                    correction_model=correction_model,
+                                    identify_model=identify_model,
+                                    output_window=True,
+                                    annotate=True
+                                )
+                                break
+
+                # start analyzing a selected input image
+                elif entry == 3:
+
+                    names = []
+
+                    # prompt user to select input video file
+                    output = "Select an input image:"
+                    i = 1
+                    for suf in ("*.png", "*.jpg"):
+                        for file in paths.INPUT_IMAGES.glob(suf):
+                            output += ("\n[" + str(i) + "] " + file.name + "\n")
+                            names.append(file.name)
+                            i += 1
+
+                    if len(names) == 0:
+                        print("No input images available.")
+                        break
+                    while True:
+                        entry = input(output)
+                        # input check
+                        if str.isdigit(entry):
+                            entry = int(entry)
+                            if 1 <= entry <= i:
+                                analysis.analyze_image(
+                                    img=paths.INPUT_IMAGES / names[entry-1],
+                                    correction_model=correction_model,
+                                    identify_model=identify_model,
+                                    output_window=True,
+                                    annotate=True
+                                )
+                                break
+                break
+    exit(1)
 
 
 if __name__ == "__main__":
